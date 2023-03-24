d587f01151fffd1c2de8e58db9f566021eeb96b5
PrakGov <55034062+prak112@users.noreply.github.com>
2023-03-24 14:10:59 +0200

    add calculator app

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 34;
+            int num2 = 86;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
