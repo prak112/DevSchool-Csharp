36e50c00c80a42752957cc6766a78da9c17e29d0
PrakGov <55034062+prak112@users.noreply.github.com>
2023-03-28 12:55:56 +0300

    area of triangle with user input

---
+++ T6_Kolmion-PintaAla/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace T6_Kolmion_Pinta_ala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+----Area of Triangle calculation----
+
+Enter Width (kanta) and Height (korkeus) of the Triangle (Kolmio):");
+            Console.WriteLine("Enter Width of Triangle (whole number/decimal) = ");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Height of Triangle (whole number/decimal) = ");
+            double height = double.Parse(Console.ReadLine());
+            double area = (width * height) / 2d;
+            Console.WriteLine($@"Area of Triangle with width = {width} and height = {height} is {area} square units
+
+---End of Calculation---");
+        }
+    }
+}
