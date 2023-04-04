9742cf98ce826449676e34cc10592468e23c02cc
Prakirth Govardhanam <prakirth112@gmail.com>
2023-04-04 20:00:25 +0300

    learn classes from Microsoft Learn tutorial

---
+++ Classes/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿namespace Classes;
+
+public class BankAccount
+{   
+    // define properties
+    public string ID { get; }
+    public string Owner { get; set; }
+    public decimal Balance 
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+            }
+            return balance;
+        } 
+    }
+
+
+    // define constructor (similar name as Class)
+    public BankAccount (string name, decimal initialBalance)
+    {      
+        //private static int accountIdSeed = 1234546756;        
+        this.ID = IDgenerator();
+        this.Owner = name;
+
+        // need to understand
+        MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");       
+    }
+
+
+    // define methods
+    // constructor method
+    public string IDgenerator ()
+    {
+        // save Random method in a variable
+        var random = new Random();
+        // convert 10-digit random number to string
+        string ID = random.Next().ToString();
+
+        return ID;
+    }
+
+    // create List in connection with Transaction Class
+    private List<Transaction> allTransactions = new List<Transaction>();
+
+
+    // deposit method
+    public void MakeDeposit (decimal amount, DateTime date, string note)
+    {   
+        // define exceptions based on rules
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Deposit amount cannot be Negative.");
+        }
+
+        // add transaction to allTransactions List
+        var deposit = new Transaction(amount, date, note);
+        allTransactions.Add(deposit);
+    }
+
+
+    // withdrawal method
+    public void MakeWithdrawal (decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Withdrawal amount cannot be Negative");
+        }
+        if (Balance < amount)
+        {
+            throw new InvalidOperationException("Insufficient Funds. Transaction not possible");
+        }
+
+        var withdrawal = new Transaction(-amount, date, note);
+        allTransactions.Add(withdrawal);
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using Classes;
+using System.Security.Principal;
+using System.Xml.Linq;
+
+
+// Tutorial soource : Microsoft Learn 
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/tutorials/classes
+
+
+// Main method
+/*void Main(string[] args)
+{
+
+    var customer = new object[3] { account.ID, account.Owner, account.Balance };
+    Console.WriteLine(customer);
+$@"New account opened.
+Account ID : {account.ID}
+Owner Name : {account.Owner}
+Account Balance : {account.Balance}");
+  
+}
+*/
+
+var account = new BankAccount("Du-Fart", 1000);
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(
+$@"New account opened.
+Account ID : {account.ID}
+Owner Name : {account.Owner}
+Account Balance : {account.Balance}");
+
+
