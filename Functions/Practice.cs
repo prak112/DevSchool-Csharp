3b1c895a3e27e92920babecdcbff7afb71e9894f
PrakGov <55034062+prak112@users.noreply.github.com>
2023-04-18 21:07:15 +0300

    basic functions, syntax and structure practice.

---
+++ Functions/Practice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+//class definition
+// class HEADER
+namespace Functions
+{
+    class Methods
+    {
+        // Mean calculation method
+        public static double Mean(int[] numbers)
+        {
+            Console.WriteLine("\nThis method calculates Mean from given Numbers");
+            int totalCount = numbers.Count();
+            double sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            double mean = sum/totalCount;         
+            return mean;
+        }
+
+
+        // SaySomething method
+        public static void SaySomething(string message)
+        {
+            Console.WriteLine($"\nThis method is called from Main\n{message.ToUpper()}\n");
+        }
+
+
+        // Area of Triangle method
+        public static double TriangleArea(double width, double height)
+        {
+            double area = 0.5 * width * height;
+            return area;
+
+        }
+
+
+        // Tax Calculator method
+        public static (double, double) TaxCalculator(double grossSalary, double taxRate)
+        {
+            // define return output Tuple 
+            (double, double) taxAndSalary = (0, 0);
+
+            // convert taxRate to percentage
+            taxRate = taxRate / 100d;
+            double paidTax = grossSalary * taxRate;
+
+            // calculation and assignment to result Tuple
+            taxAndSalary.Item1 = Math.Round((grossSalary - paidTax), 2);
+            taxAndSalary.Item2 = Math.Round(paidTax, 2);
+
+            return taxAndSalary;
+        }
+
+
+        public static (double, string) CaloriesAndJoules(double value, string valueUnit)
+        {
+            // define return output Tuple
+            (double, string) conversion = (0, "");
+
+            // calculation using Ternary operator, pack Tuple with results
+            conversion.Item1 = (valueUnit == "c") ? Math.Round((value * 0.2390), 2) : Math.Round((value * 4.184), 2);
+            conversion.Item2 = (valueUnit == "c") ? "Calories" : "Joules";
+
+            return conversion;
+
+        }
+
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,94 @@
+﻿using Functions; // namespace label of Practice class
+
+namespace MainProgram
+{
+    class Program
+    {        
+        // Main method
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                #region Mean
+                
+                // user input for Mean method
+                // totalNumbers given as user input
+                Console.WriteLine(" Enter total numbers to calculate Mean : ");
+                int totalNumbers = int.Parse(Console.ReadLine());
+
+                // initialize array
+                int[] numArray = new int[totalNumbers];
+
+                // iterate for user input based on totalNumbers
+                Console.WriteLine($"Please enter {totalNumbers} integers");
+                for (int i = 0; i < totalNumbers; i++)
+                {
+                    Console.Write($"Integer {i+1} : ");
+                    numArray[i] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("Mean of given numbers : " + Methods.Mean(numArray));
+
+                #endregion
+
+                #region SaySomething
+
+                // user input for SaySomething method
+                Console.Write("\n\nSay Something to call method and display : ");
+                string message = Console.ReadLine();
+                Methods.SaySomething(message);
+
+                #endregion
+
+                #region TriangleArea
+
+                Console.WriteLine("\n---Area of Trianlge---\nEnter base length of Triangle : ");
+                double width = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter height of Triangle : ");
+                double height = double.Parse(Console.ReadLine());
+                double area = Methods.TriangleArea(width, height);
+                Console.WriteLine($"Area of Triange : {area} sq. units");
+
+                #endregion
+
+                #region TaxCalculator
+                
+                // user input for gross taxAndSalary and tax rate
+                Console.WriteLine("\n---Welcome to Tax Calculator---\nEnter Gross Salary : ");
+                double grossSalary = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Tax Percentage: ");
+                double taxRate = double.Parse(Console.ReadLine());
+
+                // call function with a double Tuple
+                (double, double) taxAndSalary = Methods.TaxCalculator(grossSalary, taxRate);
+                Console.WriteLine($"Your Net Salary : {taxAndSalary.Item1}\nTax Paid : {taxAndSalary.Item2}");
+
+                #endregion
+
+                #region CaloriesAndJoules
+
+                // user input
+                Console.WriteLine("\n---Welcome to Calorie-Joules converter---\nEnter energy value : ");
+                double value = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter value Units :\n-'c' for Calories\n-'j' for Joules: ");
+                string valueUnit = Console.ReadLine();
+
+                // call function
+                (double, string) conversion = Methods.CaloriesAndJoules(value, valueUnit);
+                // unpack Tuple items
+                double convertedValue = conversion.Item1;
+                string unitType = conversion.Item2;
+                Console.WriteLine($"Converted {value} to {unitType}\nAfter Conversion : {convertedValue} {unitType}");
+
+
+                #endregion
+            }
+            catch (Exception ex) { Console.WriteLine("ERROR : " + ex.Message); }
+        }
+    }
+
+}
+
+
+
