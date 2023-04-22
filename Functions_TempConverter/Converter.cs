545d94c0cfdc905b533df547167deaf03c23dbb4
PrakGov <55034062+prak112@users.noreply.github.com>
2023-04-22 17:15:54 +0300

    temperature conversion with user prompts, loops and method

---
+++ Functions_TempConverter/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Converter
+{
+	public class Methods
+	{
+        public static double TemperatureConverter(string convertTo, double conversionValue)
+        {
+            double convertedValue = 0;
+            // convert user input to Upper case
+            convertTo = convertTo.ToUpper();
+
+            // calculation based on user input
+            if (convertTo == "C") { convertedValue = ((5d / 9d) * (conversionValue - 32d)); }
+            else if (convertTo == "F") { convertedValue = ((conversionValue * 9d / 5d) + 32); }
+            else
+            {
+                convertedValue = 0;
+                Console.WriteLine("Enter a valid choice.\n");
+            }
+            return Math.Round(convertedValue, 2);
+        }
+    }
+}
---
+++ Functions_TempConverter/Program.cs
@@ -0,0 +1,37 @@
+﻿using Converter;
+
+namespace Functions
+{
+    class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("---Welcome to Temperature Converter by Functions---");
+
+                // option to exit
+                bool userExit = false;
+
+                while(!userExit)
+                {
+                    Console.WriteLine("Choose conversion unit :\r\n- 'c' converts To Celsius\r\n- 'f' converts To Fahrenheit\n");
+                    // read user input
+                    string convertTo = Console.ReadLine();
+                    Console.Write("Enter value to convert : ");
+                    double conversionValue = Convert.ToDouble(Console.ReadLine());
+
+                    // call method, pass parameters
+                    double convertedValue = Methods.TemperatureConverter(convertTo, conversionValue);
+                    // display output
+                    Console.WriteLine($"Converted Temperature value : {convertedValue}");
+
+                    // option to exit
+                    Console.WriteLine("Press 'q' to Exit or Enter to Continue");
+                    userExit = Console.ReadLine() == "q" ? true : false;
+                }
+            }
+            catch (Exception ex) { Console.WriteLine("ERROR : " + ex.Message); }
+        }
+    }    
+}
