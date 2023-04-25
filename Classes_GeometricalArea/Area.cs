073ea673d9b18884847caeeae6cf4c7fae04f79d
PrakGov <55034062+prak112@users.noreply.github.com>
2023-04-25 14:01:45 +0300

    class with methods for area, perimeter calculations

---
+++ Classes_GeometricalArea/Area.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Triangle
+{
+	// Class Properties
+	public double Base { get; set; }
+	public double Height { get; set; }
+
+	// Class Constructor
+	public Triangle(double aBase, double aHeight)
+	{
+		Base = aBase;
+		Height = aHeight;
+	}
+
+	// Class Methods
+	public double Area(double Base, double Height)
+	{
+		double area = 0.5 * Base * Height;
+		return area;
+	}
+
+	public double Perimeter(double Base)
+	{ 
+		double perimeter = Base * 3;		// assuming Equilateral Triangle, all sides equal. Hence, (base * 3)
+		return perimeter;
+	}
+
+}
+
+
+public class Square
+{
+	// Class Properties
+	public double Side { get; set; }
+	
+	// Class Constructor
+	public Square (double aSide)
+	{
+		Side = aSide;
+	}
+
+	// Class Methods
+	public double Area(double Side)
+	{
+		double area = Side * Side;
+		return area;
+	}
+
+	public double Perimeter(double Side)
+	{
+		double perimeter = 4 * Side;
+		return perimeter;
+	}
+}
---
+++ Classes_GeometricalArea/Program.cs
@@ -0,0 +1,103 @@
+﻿
+using System.Diagnostics.Metrics;
+
+namespace GeometricalArea
+{
+    class GeometricalArea
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+---Welcome to Geometrical Area of Triangle and Square (in Classes)---
+Summary :
+- Choose a shape
+- Enter dimensions
+- Get Surface Area and Perimeter
+            ");
+
+            // user options to exit
+            bool userExit = false;
+
+            // iterate until userExit = true
+            while (!userExit)
+            {
+                Console.WriteLine(@"
+Choose a shape:
+- 't' for Triangle
+- 's' for Square
+
+Press 'q' to Exit or Enter to Continue
+                ");
+                
+                string userChoice = Console.ReadLine();
+                // process userChoice
+                switch (userChoice)
+                {
+                    // Triangle block
+                    case "t":
+                        // user input
+                        Console.WriteLine("Enter dimensions of Triangle :\n1 - 'Base'\n2 - 'Height'");
+                        double triangleBase = double.Parse(Console.ReadLine());
+                        double triangleHeight = double.Parse(Console.ReadLine());
+
+                        // create Triangle object
+                        Triangle userTriangle = new Triangle(triangleBase, triangleHeight);
+                        double triangleArea = userTriangle.Area(triangleBase, triangleHeight);
+                        double trianglePerimeter = userTriangle.Perimeter(triangleBase);
+
+                        // display results
+                        Console.WriteLine($@"
+Triangle Dimensions : Base = {triangleBase} and Height = {triangleHeight}
+Triangle Surface Area : {triangleArea}
+Triangle Perimeter : {trianglePerimeter}
+
+Press 'q' to Exit or Enter to Continue
+");
+
+                        // user option to exit
+                        userExit = Console.ReadLine() == "q" ? true : false;
+                        break;
+
+                    // Square block
+                    case "s":
+                        // user input
+                        Console.WriteLine("Enter dimensions of Square :\n1 - 'Side'");
+                        double squareSide = double.Parse(Console.ReadLine());
+
+                        // create Triangle object
+                        Square userSquare = new Square(squareSide);
+                        double squareArea = userSquare.Area(squareSide);
+                        double squarePerimeter = userSquare.Perimeter(squareSide);
+
+                        // display results
+                        Console.WriteLine($@"
+Square Dimensions : Side = {squareSide}
+Square Surface Area : {squareArea}
+Square Perimeter : {squarePerimeter}
+
+Press 'q' to Exit or Enter to Continue
+");
+
+                        // user option to exit
+                        userExit = Console.ReadLine() == "q" ? true : false;
+                        break;
+
+                    // Valid input or Exit or Continue block
+                    default: 
+                        Console.WriteLine(@"
+Please enter a valid input. 
+Choose a shape :
+- 't' for Triangle
+- 's' for Square
+
+Press 'q' to Exit or Enter to Continue");
+
+                        // user option to exit
+                        userExit = Console.ReadLine() == "q" ? true : false;
+                        break;
+                }
+            }
+            Console.WriteLine("\n---Terminate Program---\n");
+        }
+    }
+}
