742702a983596f7eec2c529dc8bd783cc5731ad5
Prakirth Govardhanam <prakirth112@gmail.com>
2023-04-25 19:10:58 +0300

    initiate FuelConsumption with template

---
+++ Classes_FuelConsumption/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FuelConsumption
+{
+    class MainProgram
+    {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(@"
+---Welcom to Trip Cost Calculator (only Fuel cost)---
+Summary:
+- <PROGRAM_SUMMARY_POINT1>
+- <PROGRAM_SUMMARY_POINT2>...
+");
+        // initialize variables
+        bool userExit = false;
+
+        // application loop, until userExit = true        
+        try
+        {
+            while (!userExit)
+            {
+                // PROGRAM_CODE BELOW
+
+
+
+                // option for user to exit
+                Console.WriteLine("\nPress 'q' to exit or Enter to continue");
+                if (Console.ReadLine() == "q") { userExit = true; }
+            }
+            Console.WriteLine("\n---End <program_name>---\n");
+        }
+        catch (Exception ex) { Console.WriteLine($"ERROR : {ex.Message}\n---Terminate <PROGRAM_NAME>---\n"); }
+    }
+}
+}
+
