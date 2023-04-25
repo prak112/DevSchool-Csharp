d7eda3c861e495b195c43ed5729b7e94c9603cfc
Prakirth Govardhanam <prakirth112@gmail.com>
2023-04-25 18:48:20 +0300

    bi-directional fibonacci series using object oriented programming (oop)

---
+++ Classes_FibonacciSeries/FibonacciNumbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+// logic
+// base elements 0 ,1
+// FORWARD -> nth element = (n-1)th element + (n-2)th element
+// BACKWARD -> (n-2)th element = (n-1)th + nth element
+
+// class for Fibonacci logic
+public class FibonacciNumbers
+{
+    // class properties
+    public int TotalNumbers { get; set; }
+
+    // class constructor
+    public FibonacciNumbers(int aTotalNumbers)
+    {
+        TotalNumbers = aTotalNumbers;
+    }
+
+    // class Methods
+    public List<int> ForwardSeries ()
+    {
+        // create result List
+        List<int> forwardSeries = new List<int> { 0, 1};
+
+        // add sum of (n-2)th and (n-1)th element
+        for(int i = forwardSeries.Count; i <= TotalNumbers; i++)
+        {
+            int previous1 = forwardSeries[i - 1];
+            int previous2 = forwardSeries[i - 2];
+            int nextFib = previous1 + previous2;
+
+            forwardSeries.Add(nextFib);
+        }
+        return forwardSeries;
+
+    }
+
+    public List<int> BackwardSeries ()
+    {
+        // create result List
+        List<int> backwardSeries = new List<int> { -1, 0 };
+
+        // add sum of (n)th and (n-1)th element
+        for (int i = backwardSeries.Count; i <= TotalNumbers; i++)
+        {
+            int previous1 = backwardSeries[i - 1];
+            int previous2 = backwardSeries[i - 2];
+            int prevFib = previous1 + previous2;
+
+            backwardSeries.Add(prevFib);
+        }
+        return backwardSeries;
+    } 
+}
---
+++ Classes_FibonacciSeries/Program.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(@"
+---Welcome to BiDirectional Fibonacci Series---
+Summary:
+- Enter a direction :
+    - '+' for Positive Fibonacci Series
+    - '-' for Negative Fibonacci Series
+- Enter total numbers to be in series
+                                 ");
+
+                bool userExit = false;
+                while (!userExit)
+                {
+                    // user choice for forward or backward Fibonacci series
+                    Console.Write("Direction : ");
+                    string direction = Console.ReadLine();
+
+                    // total numbers in Fibonacci series
+                    Console.Write("Total Numbers in Series : ");
+                    int totalNumbers = int.Parse(Console.ReadLine());
+
+                    // create FibonacciNumbers object and pass variable
+                    FibonacciNumbers userSeries = new FibonacciNumbers(totalNumbers);
+
+                    // result list series
+                    List <int> resultList = new List <int>();
+
+                    // choose FibonacciNumber method by direction
+                    Console.WriteLine("Resulting Fibonacci Series : "); 
+                    switch (direction)
+                    {
+                        case "+":
+                            resultList = userSeries.ForwardSeries();
+                            break;
+
+                        case "-":
+                            resultList = userSeries.BackwardSeries();
+                            break;
+
+                        default:
+                            userExit = true;
+                            break;
+                    }
+                    foreach (int item in resultList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("Press 'q' to Exit or Enter to Continue");
+                    userExit = Console.ReadLine() == "q" ? true : false;
+                }           
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR : " + ex.Message);
+            }
+        }
+    }
+
+}
