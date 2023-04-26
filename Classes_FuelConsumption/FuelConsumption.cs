19d2d3a4e713e73d463cc8f8fa1505b7d9e93968
PrakGov <55034062+prak112@users.noreply.github.com>
2023-04-26 17:46:13 +0300

    add class, return list structure, update class name

---
+++ Classes_FuelConsumption/FuelConsumption.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Trip
+{
+	public string Location { get; set; }
+	public double Distance { get; set; }
+	public double FuelCost { get; set; }
+}
+
+public class FuelConsumption
+{
+    // Properties
+    public string Location { get; set; }
+    public double Distance { get; set; }
+
+	// Constructor
+	public FuelConsumption( string aLocation, double aDistance)
+	{
+		Location = aLocation;
+		Distance = aDistance;
+	}
+
+	// Methods
+	public static List<Trip> Trips 
+	{
+		List<Trip> TripsList = new List<Trip>();
+	} 
+	 
+}
