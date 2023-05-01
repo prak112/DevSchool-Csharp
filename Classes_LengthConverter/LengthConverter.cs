87af9a2ebd6ef3ffa0429b5e5acbed8a0c1cc378
PrakGov <55034062+prak112@users.noreply.github.com>
2023-05-01 12:21:27 +0300

    basic length conversion for class and methods practice

---
+++ Classes_LengthConverter/LengthConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class LengthConverter
+{
+	// Property
+	public double Length { get; set; }
+
+	// Constructor
+	public LengthConverter(double aLength)
+	{
+		Length = aLength;
+	}
+
+	// Methods
+	public double ToCentimeter()
+	{
+		double centimeter = Length * 100;
+		return centimeter;
+	}
+
+	public double ToDecimeter()
+	{
+		double decimeter = Length * 10;
+		return decimeter;
+	}
+
+	public double ToKilometer()
+	{
+		double kilometer = Length / 1000;
+		return kilometer;
+	}
+
+	public double ToInch()
+	{
+		double inch = Length * 39.37008;
+		return inch;
+	}
+
+	public double ToFoot()
+	{
+		double foot = Length * 3.28084;
+        return foot;
+    }
+
+	public double ToYard()
+	{
+        double yard = Length * 1.093613;
+        return yard;
+    }
+}
---
+++ Classes_LengthConverter/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Program
+{
+    class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+---Welcom to Length Conversion from Meter to other Length Units---
+Summary:
+- Enter Length in Metres
+- Program displays converted units in one of chosen Length Units, i.e.,
+    -Centimeter
+    -Decimetre
+    -Kilometer
+    -Inch
+    -Foot
+    -Yard
+");
+            // initialize variables
+            bool userExit = false;
+
+            // application loop, until userExit = true        
+            try
+            {
+                while (!userExit)
+                {
+                    // user input
+                    Console.Write("\nEnter any length (in Metres): ");
+                    double length = double.Parse(Console.ReadLine());
+                    Console.WriteLine(@"
+Select unit to convert to :
+    -'c' for Centimeter
+    -'d' for Decimetre
+    -'k' for Kilometer
+    -'i' for Inch
+    -'f' for Foot
+    -'y' for Yard
+");
+                    string convertTo = Console.ReadLine();
+
+                    // create LengthConverter class object, pass length
+                    LengthConverter lengthConverter = new LengthConverter(length);
+
+                    // switch block for calling method based on convertTo
+                    switch (convertTo)
+                    {
+                        case "c":
+                            Console.WriteLine($"{length} Meters = {lengthConverter.ToCentimeter()} Centimeters");
+                            break;
+                        case "d":
+                            Console.WriteLine($"{length} Meters = {lengthConverter.ToDecimeter()} Decimeters");
+                            break;
+                        case "k":
+                            Console.WriteLine($"{length} Meters = {lengthConverter.ToKilometer()} Kilometers");
+                            break;
+                        case "i":
+                            Console.WriteLine($"{length} Meters = {lengthConverter.ToInch()} Inches");
+                            break;
+                        case "f":
+                            Console.WriteLine($"{length} Meters = {lengthConverter.ToFoot()} Feet");
+                            break;
+                        case "y":
+                            Console.WriteLine($"{length} Meters = {lengthConverter.ToYard()} Yards");
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // option for user to exit
+                    Console.WriteLine("\nPress 'q' to exit or Enter to continue");
+                    if (Console.ReadLine() == "q") { userExit = true; }
+                }
+                Console.WriteLine("\n---End <program_name>---\n");
+            }
+            catch (Exception ex) { Console.WriteLine($"ERROR : {ex.Message}\n---Terminate Program---\n"); }
+        }
+    }
+}
