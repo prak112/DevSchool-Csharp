7a3e8cb6354068653f156d2c4b3cf8c3128192df
PrakGov <55034062+prak112@users.noreply.github.com>
2023-05-22 17:41:52 +0300

    refactor for sensible pricing logic

--- PracticeTest_PizzaOrder/Customer.cs
+++ PracticeTest_PizzaOrder/Customer.cs
@@ -25,31 +25,15 @@ namespace PracticeTest
             decimal price = 0;
             int toppings = customerOrder.Count();
 
-            switch (toppings)
+            if (toppings > 3)           // increment price by 1 if toppings > 3
             {
-                case  4:
-                    price = Pizza.BasePrice + 1;
-                    return price;
-
-                case 5:
-                    price = Pizza.BasePrice + 2;
-                    return price;
-
-                case 6:
-                    price = Pizza.BasePrice + 3;
-                    return price;
-
-                case 7:
-                    price = Pizza.BasePrice + 4;
-                    return price;
-
-                case > 7:
-                    price = Pizza.BasePrice + 7;
-                    return price;
-
-                default:
-                    price = Pizza.BasePrice;
-                    return price;
+                price = Pizza.BasePrice + (customerOrder.Count() - 3) ;
+                return price;
+            }
+            else                        // base price 15, if toppings < 3
+            {
+                price = Pizza.BasePrice ;
+                return price ;
             }
         }
 
